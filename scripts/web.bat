81264247190aaa96a20504c6374afd352e3eff4c
brandscill <brandonsbillingham@gmail.com>
2021-09-16 17:28:33 +0100

    Sort out webpack builds

--- scripts/web.bat
+++
@@ -1,2 +0,0 @@
-xcopy .\shared .\docs\shared /s /e /h /f /y
-webpack-dev-server --inline --config .\webpack.web.js --content-base docs\
--- webpack.web.js
+++ webpack.web.js
@@ -1,23 +1,40 @@
 var webpack = require('webpack');
+var path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const relativePath = (extraPath) => path.resolve(__dirname, extraPath);
 
 module.exports = {
   entry: './docs/main.js',
-
+  mode: "development",
   output: {
     filename: 'index.js',
-    path: 'docs'
+    path: relativePath("docs")
   },
+  
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "node_modules/pdfjs-dist/build/pdf.js", to: "shared"},
+        { from: "node_modules/pdfjs-dist/build/pdf.worker.js", to: "shared"},
+        { from: "node_modules/pdfjs-dist/web/pdf_viewer.js", to: "shared"},
+        { from: "node_modules/pdfjs-dist/web/pdf_viewer.css", to: "shared"}
+      ]
+    })
+  ],
 
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
-      }
-    ]
+      ]
+    }
+  ]
   }
 };
